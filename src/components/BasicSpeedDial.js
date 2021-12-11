d7e9af2a926da202ada0c2889420afaae8ae6cc1
unknown <philippe.soubrier@gmail.com>
2021-12-11 14:56:44 +0100

    add post function added, can't change gridLayout rn, has to be on back-end

--- src/components/BasicSpeedDial.js
+++ src/components/BasicSpeedDial.js
@@ -4,11 +4,13 @@ import SpeedDialAction from '@mui/material/SpeedDialAction';
 import PrintIcon from '@mui/icons-material/Print';
 import ShareIcon from '@mui/icons-material/Share';
 import MenuIcon from '@mui/icons-material/Menu';
-import LanguageIcon from '@mui/icons-material/Language';
 import ContactsIcon from '@mui/icons-material/Contacts';
+import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import {Link as RouterLink} from 'react-router-dom';
+import { Box } from '@mui/system';
 
 const actions = [
-  { icon: <LanguageIcon />, name: 'Language' },
+  { icon: <Box component={RouterLink} to="/add"><AddPhotoAlternateIcon sx={{mt:1}}/></Box>, name: 'Add extra post' },
   { icon: <ContactsIcon />, name: 'contact' },
   { icon: <PrintIcon />, name: 'Print' },
   { icon: <ShareIcon />, name: 'Share' },
--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,56 +1,95 @@
-import { Button, FormControl,  FormGroup, FormHelperText, FormLabel, MenuItem, Select, TextField } from '@mui/material'
+import { Button, FormGroup, FormHelperText, FormLabel, Input, MenuItem, Select, TextField } from '@mui/material'
 import { Box } from '@mui/system'
-import React from 'react'
-import { styled } from '@mui/material/styles';
-
-const Input = styled('input')({
-    display: 'none',
-  });
+import React, {useState} from 'react'
+import SaveIcon from '@mui/icons-material/Save';
+import mime from "mime-types"
 
 export default function Add() {
 
-    const [category, setCategory] = React.useState('');
-    
+    const [categorie, setCategorie] = React.useState('');
+    const [foto, setFoto] = useState(null)
+        
     const handleChange = (event) => {
-        setCategory(event.target.value);
+        setCategorie(event.target.value);
     };
-    
+
+      // upload data to solid pod
+    async function uploadData() {
+        const mimetype = mime.lookup(foto.name)
+        const naamFoto = foto.name
+        var requestOptionsPicture = {
+        method: "PUT",
+        headers: { "Content-Type": mimetype },
+        body: foto,
+        };
+
+        // parameters post
+        // heb al:
+        //  category
+        //  naamFoto
+        const date = document.getElementById("date").value
+        const naam = document.getElementById("title").value
+        const postLink = document.getElementById("postLink").value
+
+        // add picture to solid pod
+        await fetch(
+        `http://localhost:5000/Flupke3622/mydata/thumbnails/${naamFoto}`,
+        requestOptionsPicture
+        );
+
+        // alert
+        const message =
+            ",{ \n" + 
+            '    "title": "'+ naam + '",\n' + 
+            '    "link": "'+ postLink + 'embed",\n' + 
+            '    "year": "'+ date + '",\n' + 
+            '    "thumbnail": "http://localhost:5000/Flupke3622/mydata/thumbnails/'+ naamFoto + '",\n' + 
+            '    "category": "'+ categorie + '",\n' +
+            "}";
+        
+        navigator.clipboard.writeText(message);
+        alert("text copied to clipboard, add it to gridLayout.JSON");
+        window.location.href = 'http://localhost:3000';
+    }
+
     
     return (
-        <Box>
-            <FormControl sx={{ m: 5 }} component="fieldset" variant="standard">
-                <FormLabel component="legend">New post form</FormLabel>
-                <FormGroup>
+        <Box justifyContent='center' display='flex' mt='50px'>
+            <FormGroup>
+                <FormLabel component="legend" >New post form</FormLabel>
+                                   
+                <TextField id="title" label="Title of post" placeholder="Title" margin="normal"/>
+                <TextField id="postLink" label="Link to post" placeholder="Link" margin="normal"/>
+                <TextField id="date" label="Year" placeholder="Year" margin="normal"/>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={categorie}
+                    label='Category'
+                    variant="standard"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={'woodworking'}>woodworking</MenuItem>
+                    <MenuItem value={'3D printing'}>3D printing</MenuItem>
+                    <MenuItem value={'DIY'}>DIY</MenuItem>
+                    <MenuItem value={'3D'}>3D</MenuItem>
 
-
-
-                    
-                    <TextField id="title" label="Title of post" placeholder="Title" margin="normal"/>
-                    <TextField id="postLink" label="Link to post" placeholder="Link" margin="normal"/>
-                    <TextField id="year" label="Year" placeholder="Year" margin="normal"/>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={category}
-                        label='Category'
-                        onChange={handleChange}
-                    >
-                        <MenuItem value={'woodworking'}>woodworking</MenuItem>
-                        <MenuItem value={'3D printing'}>3D printing</MenuItem>
-                        <MenuItem value={'DIY'}>DIY</MenuItem>
-                        <MenuItem value={'3D'}>3D</MenuItem>
-
-                    </Select> 
-                <FormHelperText>Catregory</FormHelperText>    
-                <label htmlFor="contained-button-file" >
-                    <Input accept="image/*" id="contained-button-file" multiple type="file" />
-                    <Button variant="contained" component="span" sx={{mt:3}}>
-                        Upload thumbnail
-                    </Button>
-                </label>                       
-                </FormGroup>
-                
-            </FormControl>
+                </Select> 
+                <FormHelperText>Category</FormHelperText>    
+                <Input 
+                    type="file" 
+                    onChange={e => setFoto(e.target.files[0])}
+                    sx={{mt:2}}
+                />
+                <FormHelperText>Add " media/?size=t " to post url to get thumbnail</FormHelperText>
+                <Button 
+                    color="secondary" 
+                    startIcon={<SaveIcon />}
+                    onClick={uploadData}
+                    sx={{mt:3}}>
+                    save and copy text
+                </Button>
+            </FormGroup>
         </Box>
     )
 }
